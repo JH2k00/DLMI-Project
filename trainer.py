2d64f99c73ece3e41693b4e8f4ee90ff1e9e8ca7
JH2k00 <jadhaidamous@hotmail.com>
2024-01-17 01:17:27 +0100

    Added overall metric calculations to training loop

--- trainer.py
+++ trainer.py
@@ -1,45 +1,65 @@
 import torch
 
 def train_kc_model(model, optimizer, criterion, train_loader, val_loader, epochs, device): # TODO Scheduler
+    N_train = len(train_loader.dataset)
+    N_val = len(val_loader.dataset)
     for epoch in range(epochs):
         model.train()
+
+        TP = 0
+        P = 0
+        PP = 0
+        accuracy = 0
+        epoch_loss = 0
+
         for batch_idx, (images, labels) in enumerate(train_loader):
             optimizer.zero_grad()
             images = images.to(device)
             labels = labels.to(device)
             output = model(images)
             loss = criterion(output, labels)
-            
+
+            TP += torch.logical_and(output.argmax(dim=1).eq(1), labels.argmax(dim=1).eq(1)).sum()
+            PP += output.argmax(dim=1).eq(1).sum()
+            P += labels.argmax(dim=1).eq(1).sum()
+            accuracy += output.argmax(dim=1).eq(labels.argmax(dim=1)).sum()
+            epoch_loss += loss
+
             loss.backward()
             optimizer.step()
 
-        print("Metrics for the last batch :")
-        report_metrics(output.detach().cpu(), labels.detach().cpu(), epoch, loss.detach().cpu())
+        accuracy /= N_train
+        precision = TP / PP
+        recall = TP / P
+        epoch_loss /= len(train_loader)
+        print("Training :")
+        print("Epoch : %d, Loss : %.2f, Accuracy : %.2f, Precision : %.2f, Recall : %.2f" 
+                  %(epoch+1, loss, accuracy, precision, recall)) 
         
         model.eval()
         with torch.no_grad():
+            TP = 0
+            P = 0
+            PP = 0
+            accuracy = 0
+            epoch_loss = 0
+
             for batch_idx, (images, labels) in enumerate(val_loader):
                 images = images.to(device)
                 labels = labels.to(device)
                 output = model(images)
                 loss = criterion(output, labels)
-            print("Metrics for the last batch :")
-            report_metrics(output.detach().cpu(), labels.detach().cpu(), epoch, loss.detach().cpu())
 
-def report_metrics(output, labels, epoch, loss):
-    accuracy = output.argmax(dim=1).eq(labels.argmax(dim=1)).sum() / labels.shape[0]
-    print("Epoch : %d, Loss : %.2f, Accuracy : %.2f" 
-                  %(epoch+1, loss, accuracy), end="") 
-    N_pos_classified = output.argmax(dim=1).eq(1).sum() > 0
-    if(N_pos_classified > 0):
-        precision = torch.logical_and(output.argmax(dim=1).eq(1), labels.argmax(dim=1).eq(1)).sum() / N_pos_classified
-        print(", Precision : %.2f"%precision, end="")
-    else:
-        print(", Precision : No Data", end="")
-    N_pos = labels.argmax(dim=1).eq(1).sum() > 0
-    if(N_pos > 0):
-        recall = torch.logical_and(output.argmax(dim=1).eq(1), labels.argmax(dim=1).eq(1)).sum() / N_pos
-        print(", Recall : %.2f"%recall, end="")
-    else:
-        print(", Recall : No Data", end="")
-    print("")
+                TP += torch.logical_and(output.argmax(dim=1).eq(1), labels.argmax(dim=1).eq(1)).sum()
+                PP += output.argmax(dim=1).eq(1).sum()
+                P += labels.argmax(dim=1).eq(1).sum()
+                accuracy += output.argmax(dim=1).eq(labels.argmax(dim=1)).sum()
+                epoch_loss += loss
+            
+            accuracy /= N_val
+            precision = TP / PP
+            recall = TP / P
+            epoch_loss /= len(val_loader)
+            print("Validating :")
+            print("Epoch : %d, Loss : %.2f, Accuracy : %.2f, Precision : %.2f, Recall : %.2f" 
+                    %(epoch+1, loss, accuracy, precision, recall))
